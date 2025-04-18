3389e4d30c945f05978d366c4dddc4c4b78ab21f
Riccardo Colombo <riccardo.colombo@ext.wefox.com>
2025-04-18 21:40:19 +0200

    fix url name

--- mobile/config/api.ts
+++ mobile/config/api.ts
@@ -1,8 +1,8 @@
 import Constants from 'expo-constants'
-type ApiEnvironment = 'standard' | 'emulator' | 'remote'
+type ApiEnvironment = 'local' | 'emulator' | 'remote'
 
 const API_URLS: Record<ApiEnvironment, string> = {
-  standard: 'http://192.168.1.162:3000/api',     // IP locale standard
+  local: 'http://192.168.1.162:3000/api',     // IP locale standard
   emulator: 'http://10.0.2.2:3000/api',          // Per emulatori
   remote: 'https://bookstore-abp0.onrender.com/api'  // Backend su Render
 }
@@ -15,7 +15,7 @@ export const getApiUrl = (): string => {
     return API_URLS[apiType]
   }
 
-  return API_URLS.standard
+  return API_URLS.remote
 }
 
 export const BASE_URL = getApiUrl()
